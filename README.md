e96d38b495054d31d3ccf027099d1d316152633a
NikkiMounce <nikkimounce@gmail.com>
2021-08-19 14:50:05 -0700

    started html and working on programming index.js

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # read-me-generator
 Homework Assignment #9
+https://www.digitalocean.com/community/tutorials/nodejs-interactive-command-line-prompts
+https://www.npmjs.com/package/prompts
--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,87 +1,78 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const prompts = require('prompts');
 const fs = require('fs');
+const myConsole = new console.Console(fs.createWriteStream('./log.txt'));
 
-// TODO: Create an array of questions for user input
-var questions([
-    {
-        name: 'title',
-        question: 'What is the title of the Project?',
-        type: 'input',
-    },
-    {
-        name: 'example1',
-        question: 'Attach an image or snippet of an example of the code found in the application',
-        type: 'input',
-    },
-    {
-        name: 'features',
-        question: 'What features does your application have?',
-        type: 'input',
-    },
-    {
-        name: 'example2',
-        question: 'Attach an image or snippet of an example of the code found in the application',
-        type: 'input',
-    },
-    {
-        name: 'installation',
-        question: 'Insert the commands that can be used to install this program',
-        type: 'input',
-    },
-    {
-        name: 'built-with',
-        question: 'What technologies were used to build this application?',
-        type: 'list',
-        choices: [""]
-    },
-    {
-        name: 'prerequisites',
-        question: 'Indicate which (if any) system requirements, tools or global dependencies must be installed in order for a user to run/use code:',
-        type: 'list',
-        choices: ["Linux", "Windows", "Mac", ]
-    },
-    {
-        name: 'contributing',
-        question: 'Would you like to include instructions on how to fork the repo via GitHub?',
-        type: 'confirm',
-    },
+inquirer
+    .prompt= ([
+        {
+            name: 'title',
+            question: 'What is the title of the Project?',
+            type: 'input',
+        },
+        {
+            name: 'description',
+            question: 'Write a brief description of your application:',
+            type: 'input',
+        },
+        {
+            name: 'usage',
+            question: 'Enter usage instructions',
+            type: 'input',
+        },
+        { //checkbox
+            name: 'contributing',
+            question: 'Would you like to include instructions on how to fork the repo via GitHub?',
+            type: 'confirm',
+        },
+        {
+            name: 'test',
+            question: '',
+            type: '',
+            choices: ["]
+        },
+        { //multi-select
+            name: 'licensing',
+            question: 'Which license would you like to use for this application?',
+            type: 'list',
+            choices:["Apache v2.0", "Boost Software 1.0", "GNU GplV3", "ISC", "MIT", "Mozilla Public 2.0", "The Unlicense"]
+        },
+        { //multi-prompt
+            name: 'contact',
+            question: 'Insert your name, github link, email address',
+            type: 'list',
+        },
+    ]).then((data) => {
+        const fileName = `${data.name.toLowerCase().split(' ').join('')}.json`;
 
-    {
-        name: 'project-status',
-        question: 'Select the current phase of development for this application:',
-        type: 'input',
-    },
-    {
-        name: 'acknowledgements',
-        question: 'Indicate any persons (if any) whom you would like to acknowledge for aiding with development or providing support, etc.',
-        type: 'input',
-    },
-    {
-        name: 'contact',
-        question: 'Insert your name, github link, email address',
-        type: 'list',
-        choices:["Apache v2.0", "Boost Software 1.0", "GNU GplV3", "ISC", "MIT", "Mozilla Public 2.0", "The Unlicense"]
-    },
-    {
-        name: 'licensing',
-        question: 'Which license would you like to use for this application?',
-        type: 'list',
-        choices:["Apache v2.0", "Boost Software 1.0", "GNU GplV3", "ISC", "MIT", "Mozilla Public 2.0", "The Unlicense"]
+        fs.writeFile(fileName, JSON.stringify(data, null, '\t'), (err) =>
+            err ? console.log(err) : console.log('Success!')
+        );
+    });
+
+
+//     const container = document.querySelector(".read-me-generator");
+//     response.forEach((result) => {
+//     const content = document.createElement("div");
+//     content.classList = "sections";
+//     const layout = `
+//             <h2 class="question">${result.question}</h5>
+//             <p class="answer">${result.response}</p>`;
+
+//   container.innerHTML += layout;
 });
 
 
-.then(data => {
-   console.info('Answer:', answers);
-});
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to initialize app
+// function init() {}
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
 
 
