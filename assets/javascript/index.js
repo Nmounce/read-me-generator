cb5dd72b00d67f86b790a582f876d19e36cbf13e
NikkiMounce <nikkimounce@gmail.com>
2021-08-21 20:07:07 -0700

    fixed some functionality, finished function for handle licensing

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -15,14 +15,19 @@ inquirer
             type: 'input',
         },
         {
+            name: 'table',
+            question: 'Which topics would you like to include in the README?',
+            type: 'checkbox',
+            choices: ['Installation', 'Usage', 'Licensing', 'Contributing', 'Test', 'Questions'],
+        },
+        {
             name: 'installation',
-            question: '',
-            type: '',
-            choices: [""],
+            question: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
+            type: 'input',
         },
         {
             name: 'usage',
-            question: 'Enter usage instructions',
+            question: 'Provide instructions on how to use your project- can also include screenshots to show examples of the running project',
             type: 'input',
         },
         {
@@ -38,13 +43,8 @@ inquirer
         },
         { //fix
             name: 'test',
-            question: 'Would you like to include instructions on how to fork the repo via GitHub?',
-            type: 'confirm',
-        },
-        { //fix
-            name: 'questions',
-            question: 'Would you like to include instructions on how to fork the repo via GitHub?',
-            type: 'confirm',
+            question: 'Describe and show how to run the tests with code examples.',
+            type: 'input',
         },
         {
             name: 'name',
@@ -57,8 +57,8 @@ inquirer
             type: 'input',
         },
         {
-            name: 'insert your GitHub account',
-            question: 'Insert your name',
+            name: 'git',
+            question: 'insert your GitHub account',
             type: 'input',
         },
     ]).then((data) => {
@@ -70,10 +70,12 @@ inquirer
 
         var markdown = `# ${data.title}
         ${handleBadge(data.licensing)}
+
         ## Description
         ${data.description}
 
         ## Table of Contents
+        ${data.table}
 
         ## Installation
         ${data.install}
@@ -84,7 +86,7 @@ inquirer
         ## Licensing
         ${handleLicense(data.licensing)}
 
-        ## Contributing
+        ${handleContTable(data.contributing)}
         ${handleCont(data.contributing)}
 
         ## Tests
@@ -100,6 +102,12 @@ inquirer
         fs.writeFileSync("test.md", markdown);
     });
 
+function handleTable {
+    const selection = str.split$ {
+        data.table
+    }
+}
+
 function handleCont(info) {
     if (info === true) {
         return `Contribute to README Generator
@@ -110,6 +118,14 @@ function handleCont(info) {
                 4. Push to the original branch: git push origin <project_name/<location>
                 5. Create the pull request.
                 Alternatively, see the Github documentation on creating a pull request.`
+    } else {
+        return ""
+    }
+}
+
+function handleContTable(info) {
+    if (info === true) {
+        return '## Contributing'
     } else {
         return ""
     }
@@ -147,19 +163,19 @@ function handleLicense(info) {
     }
 }
 
-var apacheBadge = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+var apacheBadge = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]";
 var apacheLink = "https: //opensource.org/licenses/Apache-2.0";
-var apacheLicense = "Copyright'yyyy''name of copyright owner' Licensed under the Apache License, Version 2.0(the 'License'); you may not use this file except in compliance with the License. You may obtain a copy of the License at 'http: //www.apache.org/licenses/LICENSE-2.0' Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.";
+var apacheLicense = "[APACHE] Copyright'yyyy''name of copyright owner' Licensed under the Apache License, Version 2.0(the 'License'); you may not use this file except in compliance with the License. You may obtain a copy of the License at 'http: //www.apache.org/licenses/LICENSE-2.0' Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.";
 
 var creativeBadge = "[![License: CC0-1.0](https://img.shields.io/badge/License-CC0%201.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)";
 var creativeLink = "https: //creativecommons.org/publicdomain/zero/1.0/";
-var creativeLicense =  "CC0 1.0 Universal CREATIVE COMMONS CORPORATION IS NOT A LAW FIRM AND DOES NOT PROVIDE LEGAL SERVICES.DISTRIBUTION OF THIS DOCUMENT DOES NOT CREATE AN ATTORNEY - CLIENT RELATIONSHIP.CREATIVE COMMONS PROVIDES THIS INFORMATION ON AN 'AS-IS' BASIS.CREATIVE COMMONS MAKES NO WARRANTIES REGARDING THE USE OF THIS DOCUMENT OR THE INFORMATION OR WORKS PROVIDED HEREUNDER, AND DISCLAIMS LIABILITY FOR DAMAGES RESULTING FROM THE USE OF THIS DOCUMENT OR THE INFORMATION OR WORKS PROVIDED HEREUNDER.";
+var creativeLicense = "[Creative Commons] CC0 1.0 Universal CREATIVE COMMONS CORPORATION IS NOT A LAW FIRM AND DOES NOT PROVIDE LEGAL SERVICES.DISTRIBUTION OF THIS DOCUMENT DOES NOT CREATE AN ATTORNEY - CLIENT RELATIONSHIP.CREATIVE COMMONS PROVIDES THIS INFORMATION ON AN 'AS-IS' BASIS.CREATIVE COMMONS MAKES NO WARRANTIES REGARDING THE USE OF THIS DOCUMENT OR THE INFORMATION OR WORKS PROVIDED HEREUNDER, AND DISCLAIMS LIABILITY FOR DAMAGES RESULTING FROM THE USE OF THIS DOCUMENT OR THE INFORMATION OR WORKS PROVIDED HEREUNDER.";
 
 var mitBadge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
 var mitLink = "https: //opensource.org/licenses/MIT"
-var mitLicense = " Copyright 'YEAR''COPYRIGHT HOLDER' Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files(the 'Software'), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and / or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.";
+var mitLicense = "[MIT] Copyright 'YEAR''COPYRIGHT HOLDER' Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files(the 'Software'), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and / or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.";
 
 
 var unBadge = "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)"
 var unLink = "https://unlicense.org/"
-var unLicense = "This is free and unencumbered software released into the public domain.Anyone is free to copy, modify, publish, use, compile, sell, or distribute this software, either in source code form or as a compiled binary, for any purpose, commercial or non - commercial, and by any means. In jurisdictions that recognize copyright laws, the author or authors of this software dedicate any and all copyright interest in the software to the public domain.We make this dedication for the benefit of the public at large and to the detriment of our heirs and successors. We intend this dedication to be an overt act of relinquishment in perpetuity of all present and future rights to this software under copyright law. THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. For more information, please refer to 'http://unlicense.org/'.";
+var unLicense = "[Unlicense] This is free and unencumbered software released into the public domain.Anyone is free to copy, modify, publish, use, compile, sell, or distribute this software, either in source code form or as a compiled binary, for any purpose, commercial or non - commercial, and by any means. In jurisdictions that recognize copyright laws, the author or authors of this software dedicate any and all copyright interest in the software to the public domain.We make this dedication for the benefit of the public at large and to the detriment of our heirs and successors. We intend this dedication to be an overt act of relinquishment in perpetuity of all present and future rights to this software under copyright law. THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. For more information, please refer to 'http://unlicense.org/'.";
--- assets/javascript/test.md
+++ assets/javascript/test.md
@@ -1,10 +1,20 @@
-# f
+# s
+        [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]
 
         ## Description
-        f
+        s
+
+        ## Table of Contents
+        Installation,Usage,Licensing,Contributing,Test,Questions
+
+        ## Installation
+        undefined
 
         ## Usage
-        f
+        
+
+        ## Licensing
+        [APACHE] Copyright'yyyy''name of copyright owner' Licensed under the Apache License, Version 2.0(the 'License'); you may not use this file except in compliance with the License. You may obtain a copy of the License at 'http: //www.apache.org/licenses/LICENSE-2.0' Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 
         ## Contributing
         Contribute to README Generator
@@ -16,9 +26,11 @@
                 5. Create the pull request.
                 Alternatively, see the Github documentation on creating a pull request.
 
-        ## Licensing
-         Copyright 'YEAR''COPYRIGHT HOLDER' Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files(the 'Software'), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and / or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+        ## Tests
+        undefined
 
-        ## Contact
-        true
+        ## Questions
+        
+        
+        
         
